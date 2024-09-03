45362e009753d2cb18ac8bc2ebe1df4496a13ad1
SuhaShahid <suhashahid.123@gmail.com>
2024-09-03 17:14:48 +0500

    70% complete

--- Table/menu.js
+++ Table/menu.js
@@ -1,21 +1,24 @@
 let productsData = [];
 
+let currentPage = 1;
+let itemsPerPage = 20;
+
 function fetchData() {
-    fetch('https://dummyjson.com/products?limit=0')
-        .then(response => response.json())
-        .then(data => {
-            productsData = data.products;
-            displayTable(productsData);
-        })
-        .catch(error => console.error('Error fetching the data:', error));
+  fetch("https://dummyjson.com/products?limit=0")
+    .then((response) => response.json())
+    .then((data) => {
+      productsData = data.products;
+      displayTable(productsData);
+    })
+    .catch((error) => console.error(error));
 }
 
 function displayTable(data) {
-    const tableBody = document.getElementById('table-body');
+  const tableBody = document.getElementById("table-body");
 
-    let tableContent = '';
-    data.forEach(product => {
-        tableContent += `
+  let tableContent = "";
+  data.forEach((product) => {
+    tableContent += `
             <tr>
                 <td>${product.id}</td>
                 <td>${product.title}</td>
@@ -24,19 +27,35 @@ function displayTable(data) {
                 <td>${product.rating}</td>
             </tr>
         `;
-    });
+  });
 
-    tableBody.innerHTML = tableContent;
+  tableBody.innerHTML = tableContent;
 }
 
 function filterTable() {
-    const searchValue = document.getElementById('search').value.toLowerCase();
-    const filteredData = productsData.filter(product =>
-        product.title.toLowerCase().includes(searchValue)
-    );
-    displayTable(filteredData);
+  const searchValue = document.getElementById("search").value.toLowerCase();
+  const filteredData = productsData.filter((product) =>
+    product.title.toLowerCase().includes(searchValue)
+  );
+  displayTable(filteredData);
 }
 
-document.getElementById('search').addEventListener('input', filterTable);
+document.getElementById("search").addEventListener("input", filterTable);
 
 fetchData();
+
+function updatePage() {}
+
+function addPagination() {
+  let pageDiv = document.createElement("div");
+  let body1 = document.getElementsByTagName('body')
+  body1.appendChild(pageDiv)
+  let pageNo = document.createElement("a");
+  let log8 = pageDiv.appendChild(pageNo);
+
+  // for (let index = 0; index < data.length; index++) {
+  //     pageNo;
+  // }
+
+  console.log(log8);
+}
