0f2f2840c37ba6160d238dcaec1a12c475818818
SuhaShahid <suhashahid.123@gmail.com>
2024-09-04 14:06:22 +0500

    almost complete

--- Table/menu.js
+++ Table/menu.js
@@ -33,6 +33,10 @@ function generateUrl() {
 function displayTable(data) {
   const tableBody = document.getElementById("table-body");
 
+  if (!data.length) {
+      tableBody.innerHTML = "<tr><td colspan='5'>No results found</td></tr>";
+      return;
+    }
   let tableContent = "";
   data.forEach((product) => {
     tableContent += `
@@ -102,100 +106,3 @@ document.getElementById("dropdown").addEventListener("change", () => {
 
 fetchData(generateUrl());
 fetchCategories();
-
-// let productsData = [];
-
-// let currentPage = 1;
-// const itemsPerPage = 20;
-// const content = document.querySelector(".content");
-
-// function fetchData() {
-//   fetch("https://dummyjson.com/products?limit=0")
-//     .then((response) => response.json())
-//     .then((data) => {
-//       productsData = data.products;
-//       displayTable(productsData);
-//     })
-//     .catch((error) => console.error(error));
-// }
-
-// function displayTable(data) {
-//   const tableBody = document.getElementById("table-body");
-
-//   const startIndex = (currentPage - 1) * itemsPerPage;
-//   const endIndex = startIndex + itemsPerPage;
-//   const pageItems = data.slice(startIndex, endIndex);
-
-//   let tableContent = "";
-//   pageItems.forEach((product) => {
-//     tableContent += `
-//       <tr>
-//         <td>${product.id}</td>
-//         <td>${product.title}</td>
-//         <td>${product.price}</td>
-//         <td>${product.category}</td>
-//         <td>${product.rating}</td>
-//       </tr>
-//     `;
-//   });
-
-//   tableBody.innerHTML = tableContent;
-
-//   addPagination(data.length);
-// }
-
-// function filterTable() {
-//   const searchValue = document.getElementById("search").value.toLowerCase();
-//   const filteredData = productsData.filter((product) =>
-//     product.title.toLowerCase().includes(searchValue)
-//   );
-//   displayTable(filteredData);
-// }
-
-// document.getElementById("search").addEventListener("input", filterTable);
-
-// fetchData();
-
-// function updatePage(pageNumber) {
-//   currentPage = pageNumber;
-//   displayTable(productsData);
-// }
-
-// function addPagination(totalItems) {
-//   const totalPages = Math.round(totalItems / itemsPerPage);
-//   const paginationContainer = document.getElementById("pagination");
-
-//   paginationContainer.innerHTML = "";
-
-//   for (let i = 0; i < totalPages; i++) {
-//     const pageButton = document.createElement("button");
-//     pageButton.innerHTML = i + 1;
-//     pageButton.addEventListener("click", () => {
-//       updatePage(i + 1);
-//     });
-//     paginationContainer.appendChild(pageButton);
-//   }
-// }
-
-// if (!data.length) {
-//   tableBody.innerHTML = "<tr><td colspan='5'>No results found</td></tr>";
-//   return;
-// }
-
-// function sortByCategory() {
-//   let dropDownMenu = document.getElementById("dropdown");
-
-//   fetch("https://dummyjson.com/products/categories")
-//     .then((res) => res.json())
-//     .then((categories) => {
-//       let options = `<option value="">Select Category </option>`;
-//       console.log(categories);
-
-//       categories.forEach((category) => {
-//         options += `<option value="${category.slug}">${category.name}</option>`;
-//       });
-//       dropDownMenu.innerHTML = options;
-//     })
-//     .catch((error) => console.error(error));
-// }
-// sortByCategory();
